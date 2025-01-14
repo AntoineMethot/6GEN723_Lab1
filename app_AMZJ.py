dfddf159281a598831b3ef0cc3915a8b9e42692e
ANTOINE METHOT <amethot@etu.uqac.ca>
2025-01-14 16:01:34 -0500

    Starting to work

--- app_AMZJ.py
+++ app_AMZJ.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     #XXX
-    return '<h2>Hello, World!, From AMZJ: 172.19.137.193</h2>'
+    return '<h2>Hello, World!, From AMZJ: 172.16.14.33</h2>'
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',debug=True,port=3000)
